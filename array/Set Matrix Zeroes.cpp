8383b21746308670fda7116a1e539884df6cf387
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-05-21 15:41:29 +0530

    Update Set Matrix Zeroes.cpp

--- array/Set Matrix Zeroes.cpp
+++ array/Set Matrix Zeroes.cpp
@@ -33,3 +33,55 @@ public:
         
     }
 };
+
+********************************************************************************
+/*without using extra space 
+if we found any block with zero then we mark it's starting row and column with zero and for matrix[0][0] take another boolean variable*/
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        
+        bool iscol=false;
+        for(int i=0;i<m;i++)
+        {
+            if(matrix[i][0]==0) iscol=true;
+        
+        
+            for(int j=1;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(matrix[0][0]==0)
+        {
+            for(int j=0;j<n;j++)
+            {
+                matrix[0][j]=0;
+            }
+        }
+        if(iscol)
+        {
+            for(int i=0;i<m;i++)
+            {
+                matrix[i][0]=0;
+            }
+        }   
+    }
+};
